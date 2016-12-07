4bd1354a0aae3358cb5aa06ec5e81c81faa6f37b
Miroslav Bagljas <bagljash@gmail.com>
2016-12-07 17:08:27 +0100

    Updated names of the ostypes throughout the code - issue #1

--- api/dc/base/serializers.py
+++ api/dc/base/serializers.py
@@ -275,9 +275,9 @@ class DcSettingsSerializer(s.InstanceSerializer):
                                       help_text=_('Whether to enable support for SunOS zones in '
                                                   'this virtual datacenter.'))
     VMS_VM_OSTYPE_DEFAULT = s.IntegerChoiceField(label='VMS_VM_OSTYPE_DEFAULT', choices=Vm.OSTYPE,
-                                                 help_text=_('Default operating system type. One of: 1 - Linux, '
-                                                             '2 - SunOS, 3 - BSD, 4 - Windows, 5 - SunOS Zone,'
-                                                             '6 - Linux Zone.'))
+                                                 help_text=_('Default operating system type. One of: 1 - Linux VM, '
+                                                             '2 - SunOS VM, 3 - BSD VM, 4 - Windows VM, '
+                                                             '5 - SunOS Zone, 6 - Linux Zone.'))
     VMS_VM_MONITORED_DEFAULT = s.BooleanField(label='VMS_VM_MONITORED_DEFAULT',
                                               help_text=_('Controls whether server synchronization with the monitoring '
                                                           'system is enabled by default.'))
