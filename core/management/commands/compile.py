094d24c8e39f1be3cc8143368326bc5863ed10ab
Daniel Kontsek <daniel@kontsek.sk>
2016-12-28 07:48:47 +0100

    Changed default verbosity of ctl.sh compile to be less verbose - PR #28

--- core/management/commands/compile.py
+++ core/management/commands/compile.py
@@ -7,7 +7,9 @@ class Command(DanubeCloudCommand):
     help = 'Recursively byte=compile all modules in ERIGONES_HOME.'
 
     def handle(self, *args, **options):
-        rc = compile_dir(self.PROJECT_DIR, maxlevels=20, quiet=int(not self.verbose))
+        self.display('Byte-compiling all modules in %s' % self.PROJECT_DIR, color='white')
+        quiet = int(int(options.get('verbosity', self.default_verbosity)) <= self.default_verbosity)
+        rc = compile_dir(self.PROJECT_DIR, maxlevels=20, quiet=quiet)
 
         if rc == 0:
             self.display('Byte-compiled all modules in %s' % self.PROJECT_DIR, color='green')
