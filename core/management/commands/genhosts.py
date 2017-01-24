2449a6e91a625d67aefb1a161b1128e87a33ac37
YanChii <jan.poctavek@erigones.com>
2017-01-24 11:12:43 +0100

    updated genhosts to include physical nodes list (#57)

--- core/management/commands/genhosts.py
+++ core/management/commands/genhosts.py
@@ -8,10 +8,12 @@ class Command(DanubeCloudCommand):
     options = (
         CommandOption('--dc', dest='dc', default=DC_ADMIN,
                       help='List hosts from a specific virtual datacenter. Defaults to "%s".' % DC_ADMIN),
+        CommandOption('--include-nodes', action='store_true', dest='include_nodes', default=False,
+                      help='Include also list of physical nodes.'),
     )
 
-    def handle(self, dc=None, **options):
-        from vms.models import Vm
+    def handle(self, dc=None, include_nodes=False, **options):
+        from vms.models import Vm, Node
 
         qs = Vm.objects
 
@@ -19,6 +21,10 @@ class Command(DanubeCloudCommand):
             qs = qs.filter(dc__name=dc)
         else:
             qs = qs.all()
+
+        # print VMs in admin DC
+        if dc:
+            print('[%s]' % dc)
 
         for vm in qs:
             try:
@@ -32,3 +38,18 @@ class Command(DanubeCloudCommand):
                 line.append('ansible_python_interpreter=/opt/local/bin/python')
 
             print(' '.join(line))
+
+        if include_nodes:
+            # print all nodes
+            print('')
+            print('[nodes]')
+            nodes = Node.objects.all()
+
+            for node in nodes:
+                line = [
+                    node.hostname,
+                    'ansible_ssh_host=%s' % node.ip_address.ip,
+                    'ansible_python_interpreter=/opt/local/bin/python'
+                    ]
+                print(' '.join(line))
+
