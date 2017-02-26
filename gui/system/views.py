4969bf7a58d79d18f8f20f9a307c9b421d392760
Daniel Kontsek <daniel@kontsek.sk>
2017-02-26 16:18:49 +0100

    Added ``GET /system/stats`` API call

--- gui/system/views.py
+++ gui/system/views.py
@@ -4,7 +4,23 @@ from django.contrib.auth.decorators import login_required
 from gui.decorators import staff_required
 from gui.utils import collect_view_data
 from gui.system.utils import GraphItem
-from vms.models import Dc, Node, Vm
+from api.system.stats.api_views import SystemStatsView
+
+
+NODE_STATS_COLORS = {
+    'offline': '#b94a48',
+    'online': '#468847',
+    'unreachable': '#f89406',
+    'unlicensed': '#999999',
+}
+
+VM_STATS_COLORS = {
+    'notcreated': '#999999',
+    'stopped': '#b94a48',
+    'running': '#468847',
+    'frozen': '#ddffff',
+    'unknown': '#333333',
+}
 
 
 @login_required
@@ -14,36 +30,10 @@ def overview(request):
     Overview/stats of the whole system.
     """
     context = collect_view_data(request, 'system_overview')
-    context['dc'] = (
-        GraphItem('public', Dc.objects.filter(access=Dc.PUBLIC).count()),
-        GraphItem('private', Dc.objects.filter(access=Dc.PRIVATE).count()),
-    )
-    context['dc_total'] = sum(i.data for i in context['dc'])
-    context['node'] = (
-        GraphItem('offline', Node.objects.filter(status=Node.OFFLINE).count(), color='#b94a48'),
-        GraphItem('online', Node.objects.filter(status=Node.ONLINE).count(), color='#468847'),
-        GraphItem('unreachable', Node.objects.filter(status=Node.UNREACHABLE).count(), color='#f89406'),
-        GraphItem('unlicensed', Node.objects.filter(status=Node.UNLICENSED).count(), color='#999999'),
-    )
-    context['node_total'] = sum(i.data for i in context['node'])
-    context['vm'] = (
-        GraphItem('notcreated', Vm.objects.filter(status__in=(Vm.NOTCREATED,
-                                                              Vm.NOTREADY_NOTCREATED,
-                                                              Vm.CREATING,
-                                                              Vm.DEPLOYING_START,
-                                                              Vm.DEPLOYING_FINISH,
-                                                              Vm.DEPLOYING_DUMMY)).count(),
-                  color='#999999'),
-        GraphItem('stopped', Vm.objects.filter(status__in=(Vm.STOPPED, Vm.NOTREADY_STOPPED)).count(),
-                  color='#b94a48'),
-        GraphItem('running', Vm.objects.filter(status__in=(Vm.RUNNING, Vm.STOPPING, Vm.NOTREADY_RUNNING)).count(),
-                  color='#468847'),
-        GraphItem('frozen', Vm.objects.filter(status__in=(Vm.FROZEN, Vm.NOTREADY_FROZEN)).count(),
-                  color='#ddffff'),
-        GraphItem('unknown', Vm.objects.filter(status__in=(Vm.NOTREADY, Vm.ERROR)).count(),
-                  color='#333333'),
-    )
-    context['vm_total'] = sum(i.data for i in context['vm'])
+    context['stats'] = stats = SystemStatsView.get_stats()
+    stats['dcs'] = [GraphItem(label, data) for label, data in stats['dcs'].items()]
+    stats['nodes'] = [GraphItem(label, data, color=NODE_STATS_COLORS[label]) for label, data in stats['nodes'].items()]
+    stats['vms'] = [GraphItem(label, data, color=VM_STATS_COLORS[label]) for label, data in stats['vms'].items()]
 
     return render(request, 'gui/system/overview.html', context)
 
