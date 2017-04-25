9827858c0d8109215cdbeec1348f35a3a5c46f22
Daniel Kontsek <daniel@kontsek.sk>
2017-04-25 21:21:17 +0200

    Fixed PEP8 inspection findings

--- ans/upgrade/mgmt/v2.5.3/files/pdns_sync_domainmetadata.py
+++ ans/upgrade/mgmt/v2.5.3/files/pdns_sync_domainmetadata.py
@@ -5,9 +5,9 @@ from pdns.models import Domain
 query = "INSERT INTO domainmetadata (domain_id, kind, content) VALUES (%s, 'ALLOW-AXFR-FROM', 'AUTO-NS')"
 
 with connections["pdns"].cursor() as cursor:
-    cursor.execute('SELECT DISTINCT domain_id FROM domainmetadata');
+    cursor.execute('SELECT DISTINCT domain_id FROM domainmetadata')
     ok_domains = [row[0] for row in cursor.fetchall()]
-    affected_domains = Domain.objects.exclude(id__in=ok_domains).values_list('id', flat=True);
+    affected_domains = Domain.objects.exclude(id__in=ok_domains).values_list('id', flat=True)
 
     for domain_id in affected_domains:
         cursor.execute(query, [domain_id])
