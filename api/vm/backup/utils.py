daf1bd223604de7df103093255de689d98b49b43
Daniel Kontsek <daniel@kontsek.sk>
2017-05-31 13:50:53 +0200

    Added ability to automatically fix pending status of finished snapshots/backups

--- api/vm/backup/utils.py
+++ api/vm/backup/utils.py
@@ -1,9 +1,19 @@
+from django.db.utils import DatabaseError
+
+from que.tasks import get_task_logger
 from api.exceptions import ObjectNotFound, InvalidInput, APIError
 from api.vm.snapshot.utils import filter_snap_define
 from vms.models import Backup
 
+logger = get_task_logger(__name__)
+
 # A backup task can wait for a free backup worker for up to 4 hours (bug #chili-584)
 BACKUP_TASK_EXPIRES = 14400
+
+try:
+    t_long = long
+except NameError:
+    t_long = int
 
 
 # noinspection PyUnusedLocal
@@ -114,3 +124,60 @@ def get_backup_cmd(action, bkp, bkps=None, define=None, zfs_filesystem=None, fsf
         raise APIError('Invalid backup action')
 
     return 'esbackup %s-%s %s' % (action_prefix, action, ' '.join(cmd_args))
+
+
+def is_backup_task_running(obj):
+    """Return True if a PUT/POST/DELETE backup task is running for a VM"""
+    bkp_tasks = (obj.get_tasks(match_dict={'view': 'vm_backup'}) +
+                 obj.get_tasks(match_dict={'view': 'vm_backup_list'}))
+
+    return any(t.get('method', '').upper() in ('POST', 'PUT', 'DELETE') for t in bkp_tasks)
+
+
+def sync_backups(db_backups, node_snaps):
+    """Sync DB status of dataset backups with real information from compute node.
+    Used by PUT vm_snapshot_list and PUT node_vm_snapshot_list."""
+    lost = 0
+
+    for bkp in db_backups:
+        if bkp.type == bkp.FILE:
+            continue
+
+        bkp_snap_name = bkp.snap_name
+
+        try:
+            if bkp_snap_name in node_snaps:
+                if bkp.status == bkp.LOST:
+                    logger.warn('Backup %s (ID %s) found, changing status to OK', bkp, bkp.id)
+                    bkp.status = bkp.OK
+                    bkp.size = t_long(node_snaps[bkp_snap_name][1])
+                    bkp.save(update_fields=('status', 'status_change', 'size'), force_update=True)
+                else:
+                    logger.debug('Backup %s (ID %s) is OK', bkp, bkp.id)
+                    if bkp.locked:  # PENDING or ROLLBACK status
+                        logger.warn('Backup %s (ID %s) is OK on compute node, but in %s status since %s',
+                                    bkp, bkp.id, bkp.get_status_display(), bkp.status_change)
+                        if is_backup_task_running(bkp.vm or bkp.node):
+                            logger.warn('Ignoring backup %s (ID %s) because a snapshot task is running on node %s',
+                                        bkp, bkp.id, bkp.node)
+                        else:
+                            logger.warn('Changing backup %s (ID %s) status to OK', bkp, bkp.id)
+                            bkp.status = bkp.OK
+                            bkp.save(update_fields=('status', 'status_change'), force_update=True)
+            else:
+                logger.warn('Backup %s (ID %s) does not exist on compute node', bkp, bkp.id)
+                if is_backup_task_running(bkp.vm or bkp.node):
+                    logger.warn('Ignoring backup %s (ID %s) because a backup task is running on node %s',
+                                bkp, bkp.id, bkp.node)
+                    continue
+                if bkp.status != bkp.LOST:
+                    bkp.status = bkp.LOST
+                    bkp.save(update_fields=('status', 'status_change'), force_update=True)
+                    lost += 1
+                continue
+        except DatabaseError:
+            logger.warn('Backup %s (ID %s) could not be updated (maybe it vanished)', bkp, bkp.id)
+
+        node_snaps.pop(bkp_snap_name, None)
+
+    return lost
