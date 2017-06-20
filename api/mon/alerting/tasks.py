082e07316d2620af38b28c932816d59151f40584
Pavol Kincel <pavol.kincel@talentsquare.com>
2017-06-20 20:19:08 +0200

    formatting

--- api/mon/alerting/tasks.py
+++ api/mon/alerting/tasks.py
@@ -13,10 +13,10 @@ logger = get_task_logger(__name__)
 # noinspection PyUnusedLocal
 @cq.task(name='api.mon.base.tasks.mon_user_group_changed', base=MonInternalTask)  # logging will be done separately
 def mon_user_group_changed(task_id, sender, group_name=None, dc_name=None, *args, **kwargs):
-    if dc_name and group_name:
+    if dc_name and group_name:  # Particular group under dc changed
         dc = Dc.objects.get_by_name(dc_name)
         zabbix = getZabbix(dc)
-        # particular group under dc changed
+
         try:
             group = Role.objects.get(dc=dc, name=group_name)
         except Role.DoesNotExist:
@@ -25,9 +25,8 @@ def mon_user_group_changed(task_id, sender, group_name=None, dc_name=None, *args
         else:
             logger.debug('Going to update %s from %s.', group.name, dc.name)
             zabbix.synchronize_user_group(group=group)
+    elif dc_name:  # Something about dc changed
 
-    elif dc_name:
-        # dc changed
         try:
             dc = Dc.objects.get_by_name(dc_name)
         except Dc.DoesNotExist:
@@ -37,14 +36,14 @@ def mon_user_group_changed(task_id, sender, group_name=None, dc_name=None, *args
         else:
             zabbix = getZabbix(dc)
             zabbix.synchronize_user_group(dc_as_group=True)  # DC name is implied by the zabbix instance
+    elif group_name:  # A group under unknown dc changed
+        # This is an expensive operation, but not called often
 
-    elif group_name:
-        # group under all dcs changed
-        # This is an expensive operation, but it's related only to a few superadmin related cases
         try:
             group = Role.objects.get(name=group_name)
         except Role.DoesNotExist:
             # group does not exist-> remove from all dcs as we don't know where it was
+
             for dc in Dc.objects.all():
                 logger.debug('Going to delete %s from %s.', group_name, dc.name)
                 zabbix = getZabbix(dc)
@@ -87,6 +86,7 @@ def mon_user_changed(task_id, sender, user_name, dc_name=None, affected_groups=(
     try:
         user = User.objects.get(username=user_name)
     except User.DoesNotExist:
+
         if dc_name:
             dc = Dc.objects.get_by_name(dc_name)
             zabbix = getZabbix(dc)
@@ -95,6 +95,7 @@ def mon_user_changed(task_id, sender, user_name, dc_name=None, affected_groups=(
         elif affected_groups:
             logger.debug('Going to delete user with name %s '
                          'from zabbixes related to groups %s.', user_name, affected_groups)
+
             for dc in Dc.objects.filter(roles__in=affected_groups):
                 zabbix = getZabbix(dc)
                 try:
@@ -104,6 +105,7 @@ def mon_user_changed(task_id, sender, user_name, dc_name=None, affected_groups=(
         else:
             logger.debug('As we don\'t know where does the user %s belonged to, '
                          'we are trying to delete it from all available zabbixes.', user_name)
+
             for dc in Dc.objects.all():  # Nasty
                 zabbix = getZabbix(dc)
                 try:
@@ -118,6 +120,7 @@ def mon_user_changed(task_id, sender, user_name, dc_name=None, affected_groups=(
             zabbix.synchronize_user(user=user)
         elif affected_groups:
             logger.debug('Going to create/update user %s in zabbixes related to groups %s.', user_name, affected_groups)
+
             for dc in Dc.objects.filter(roles__in=affected_groups):
                 zabbix = getZabbix(dc)
                 try:
@@ -127,6 +130,7 @@ def mon_user_changed(task_id, sender, user_name, dc_name=None, affected_groups=(
         else:
             logger.debug('Going to create/update user %s '
                          'in zabbixes related to all groups to which the user is related to.', user_name)
+
             for dc in Dc.objects.filter(roles__user=user):
                 zabbix = getZabbix(dc)
                 try:
