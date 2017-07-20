b60277fc589807ef40ab0fbc58e0f0fb32758827
YanChii <jan.poctavek@erigones.com>
2017-07-20 17:20:33 +0200

    added more checking when adding items to cfgdb during update

--- ans/upgrade/cfgdb/v2.6.0/files/update_master_ips_in_cfgdb.sh
+++ ans/upgrade/cfgdb/v2.6.0/files/update_master_ips_in_cfgdb.sh
@@ -1,7 +1,16 @@
 
 for vm in mgmt mon cfgdb dns img; do
-    query_cfgdb creater /esdc/vms/esdc-${vm}/master/id "1"
-    VM_IP="$(query_cfgdb get /esdc/vms/esdc-${vm}/hosts/1/ip)"
-    query_cfgdb creater /esdc/vms/esdc-${vm}/master/ip "${VM_IP}"
+	CURR_VAL="$(query_cfgdb get "/esdc/vms/esdc-${vm}/master/id" 2>/dev/null)"
+	if [[ -z "${CURR_VAL}" ]]; then
+		query_cfgdb creater "/esdc/vms/esdc-${vm}/master/id" "1"
+	fi
+
+	VM_IP="$(query_cfgdb get "/esdc/vms/esdc-${vm}/hosts/1/ip")"
+	CURR_VAL="$(query_cfgdb get "/esdc/vms/esdc-${vm}/master/ip" 2>/dev/null)"
+	if [[ -z "${CURR_VAL}" ]]; then
+		query_cfgdb creater "/esdc/vms/esdc-${vm}/master/ip" "${VM_IP}"
+	else
+		query_cfgdb set "/esdc/vms/esdc-${vm}/master/ip" "${VM_IP}"
+	fi
 done
 
