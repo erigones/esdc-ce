174ec7b3257e811824be729e69d1d0dbdeeacb14
Pavol Kincel <pavol.kincel@talentsquare.com>
2017-07-26 10:37:24 +0200

    artefact

--- ans/upgrade/node/v2.6.1/main.sh
+++ ans/upgrade/node/v2.6.1/main.sh
@@ -5,6 +5,5 @@ set -e
 ERIGONES_HOME="${ERIGONES_HOME:-"/opt/erigones"}"
 VERSION_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
 
-# https://github.com/erigones/esdc-ce/issues/129
 echo "Uninstalling librabbitmq"
 ${ERIGONES_HOME}/bin/ctl.sh pip_uninstall
