baec18caea0b06cdf1b87291299dc75b8a730832
Daniel Kontsek <daniel@kontsek.sk>
2017-07-26 22:13:25 +0200

    Updated cfgdb_disco_server from erigones/esdc-factory#64

--- ans/upgrade/cfgdb/v2.6.1/files/cfgdb_disco_server.py
+++ ans/upgrade/cfgdb/v2.6.1/files/cfgdb_disco_server.py
@@ -10,22 +10,24 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
+def get_dc_name():
+    try:
+        eprint('Determining datacenter name...')
+        dc_name = check_output(['query_cfgdb', 'get', '/esdc/settings/dc/datacenter_name']).strip()
+    except (OSError, CalledProcessError) as exc:
+        eprint('%s' % exc)
+        dc_name = '<unknown>'
+
+    eprint('Got datacenter name: "%s"' % dc_name)
+    return dc_name
+
+
 PORT = 5430
 BUFFER_SIZE = 1024
-
-try:
-    eprint('Determining datacenter name...')
-    DC_NAME = check_output(['query_cfgdb', 'get', '/esdc/settings/dc/datacenter_name']).strip()
-except (OSError, CalledProcessError) as exc:
-    eprint('%s' % exc)
-    DC_NAME = '<unknown>'
-
-eprint('Got datacenter name: "%s"' % DC_NAME)
-eprint('Running discovery server on port %s' % PORT)
-
 DISCO_MESSAGE = 'cfgdb_discovery'
-DISCO_REPLY = 'cfgdb_reply:%s' % DC_NAME
+DISCO_REPLY = None
 
+eprint('Running discovery server on port %s' % PORT)
 s = socket(AF_INET, SOCK_DGRAM)  # Create UDP socket
 s.bind(('', PORT))
 
@@ -33,5 +35,8 @@ while True:
     data, addr = s.recvfrom(BUFFER_SIZE)  # Wait for a packet
 
     if data.startswith(DISCO_MESSAGE):
+        if DISCO_REPLY is None:
+            DISCO_REPLY = 'cfgdb_reply:%s' % get_dc_name()
+
         eprint('Got discovery request from %s, sending reply...' % addr[0])
         s.sendto(DISCO_REPLY, addr)
