283e1225466390264e88395f34efc7ea2889ffbf
Pavol Kincel <pavol.kincel@talentsquare.com>
2017-07-28 14:36:47 +0200

    We want to catch the probably appropriate error on the lowest level possible. #207

--- core/management/commands/pip_uninstall.py
+++ core/management/commands/pip_uninstall.py
@@ -6,12 +6,19 @@ class Command(DanubeCloudCommand):
     option_list = (
         CommandOption('-p', '--package', action='store', dest='package', default='',
                       help='Package to be uninstalled from the virtual environment'),
+        CommandOption('-s', '--silence-errors', action='store_true', dest='silence_errors', default=False,
+                      help='Do not propagate pip errors.'),
     )
 
-    def pip_uninstall(self, package, params='-y'):
-        self.local('pip uninstall %s %s' % (params, package))
-        self.display('%s have been successfully uninstalled.\n\n ' % package, color='green')
+    def pip_uninstall(self, package, raise_on_error, params='-y'):
+        erroneous_return_code = self.local('pip uninstall %s %s' % (params, package), raise_on_error=raise_on_error)
+        if erroneous_return_code:
+            self.display('An error has occured while uninstalling %s!\n\n ' % package, color='red')
+            if not raise_on_error:
+                self.display('The library may be uninstalled already, we suppress the error.\n', color='magenta')
+        else:
+            self.display('%s have been successfully uninstalled.\n\n ' % package, color='green')
 
     def handle(self, **options):
         with lcd(self.PROJECT_DIR):
-            self.pip_uninstall(options['package'])
+            self.pip_uninstall(options['package'], raise_on_error=not options['silence_errors'])
