1cd3aec97ada8c107b6e5dbf6fe8a62a9462d20d
Pavol Kincel <pavol.kincel@talentsquare.com>
2017-07-31 14:46:08 +0200

    No packages is an incorrect condition. + typos #207

--- core/management/commands/pip_uninstall.py
+++ core/management/commands/pip_uninstall.py
@@ -1,11 +1,10 @@
-from ._base import DanubeCloudCommand, lcd, CommandOption
-
+from ._base import DanubeCloudCommand, lcd, CommandOption, CommandError
+import sys
 
 class Command(DanubeCloudCommand):
     help = 'Shortcut for pip uninstall within the application\'s virtual environment.'
-    args = '<package1> <package2> ...'
-    missing_args_message = ("No database fixture specified. Please provide the "
-                            "path of at least one fixture in the command line.")
+    args = '<package1> [package2] ...'
+
     option_list = (
         CommandOption('-s', '--silence-errors', action='store_true', dest='silence_errors', default=False,
                       help='Do not propagate pip errors.'),
@@ -14,7 +13,7 @@ class Command(DanubeCloudCommand):
     def pip_uninstall(self, package, raise_on_error, params='-y'):
         erroneous_return_code = self.local('pip uninstall %s %s' % (params, package), raise_on_error=raise_on_error)
         if erroneous_return_code:
-            self.display('An error has occured while uninstalling %s!\n\n ' % package, color='red')
+            self.display('An error has occurred while uninstalling %s!\n\n ' % package, color='red')
             if not raise_on_error:
                 self.display('The library may be uninstalled already, we suppress the error.\n', color='magenta')
         else:
@@ -22,7 +21,7 @@ class Command(DanubeCloudCommand):
 
     def handle(self, *packages, **options):
         if not packages:
-            self.display('No packages selected for removal.')
+            raise CommandError('No packages selected for removal.')
 
         with lcd(self.PROJECT_DIR):
             for package in packages:
