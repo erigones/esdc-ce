8ce1d231f5d3390156b2ad60135fc20fc3bbbb64
Daniel Kontsek <daniel@kontsek.sk>
2017-08-01 07:56:56 +0200

    Updated message

--- ans/upgrade/node/v2.6.1/main.sh
+++ ans/upgrade/node/v2.6.1/main.sh
@@ -6,5 +6,5 @@ ERIGONES_HOME="${ERIGONES_HOME:-"/opt/erigones"}"
 VERSION_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
 
 # https://github.com/erigones/esdc-ce/issues/207
-echo "Uninstalling librabbitmq"
+echo "+ Uninstalling librabbitmq"
 ${ERIGONES_HOME}/bin/ctl.sh pip_uninstall librabbitmq --silence-errors
