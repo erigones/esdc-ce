fd43b9c464b7077c0b09dcb3ba896087332ad5a3
Daniel Kontsek <daniel@kontsek.sk>
2017-08-02 15:15:27 +0200

    Added status update button to compute node list
    The user can now change the status of multiple compute nodes with one
    click. This is useful for compute node updates where it is necessary to
    have a compute node in maintenance state before running any update.

--- gui/static/gui/js/node/node-list.js
+++ gui/static/gui/js/node/node-list.js
@@ -1,10 +1,18 @@
 var NODE_LIST = null;
-function NodeList() {
+function NodeList(custsort) {
   var self = this;
   var et = new eTable('#node_list');
   var node_search = $('#node_search');
-  var controls = {};
+  var controls = {
+    'node_status': $('#node_status'),
+    'node_system_update': $('#node_system_update'),  // only in system maintenance view
+  };
+  this.modal = null;
   this.controls = controls;
+
+  if (typeof(custsort) === 'undefined') {
+    custsort = {'formatted-num': [5, 6, 7, 8, 9]};
+  }
 
   // Check if button is disabled. If not disable it for 0.5 second
   function check_btn(btn) {
@@ -18,6 +26,26 @@ function NodeList() {
     }, 500);
 
     return true;
+  }
+
+  function node_control_update_all() {
+    var selected_node_statuses = _.uniq(_.map(et.selected, function(data, hostname) {
+      return $(jq('node_label_' + hostname)).data('status_display');
+    }));
+
+    if (!selected_node_statuses.length || _.contains(selected_node_statuses, 'unlicensed')) {
+      controls.node_status.addClass('disabled');
+    } else {
+      controls.node_status.removeClass('disabled');
+    }
+
+    if (controls.node_system_update.length) {
+      if (selected_node_statuses.length === 1 && selected_node_statuses[0] === 'maintenance') {
+        controls.node_system_update.removeClass('disabled');
+      } else {
+        controls.node_system_update.addClass('disabled');
+      }
+    }
   }
 
   this.get_hostnames = function() {
@@ -36,12 +64,13 @@ function NodeList() {
   // Non-empty selection
   et.controls_show = function() {
     _.each(controls, function(value, key, list) { value.removeClass('disabled'); });
+    node_control_update_all();
   };
 
   SIGNALS.view_node_list.dispatch(this);
 
   // **** Table init ****
-  obj_list_sort_js(et.elements.table, [0], {'formatted-num': [5, 6, 7, 8, 9]});
+  obj_list_sort_js(et.elements.table, [0], custsort);
   et.toggle_controls();
 
   // **** Node search ****
@@ -70,4 +99,18 @@ function NodeList() {
       });
     }
   }); // Node search
+
+  // **** Node actions ****
+  controls.node_status.click(function() {
+    self.modal = new obj_form_modal($(this), '#node_status_modal', function(mod, start) {
+      if (start) {
+        var hostnames = self.get_hostnames();
+        $('#id_hostnames_text').html(hostnames.join(', '));
+        $('#id_hostnames').val(hostnames);
+      }
+    });
+
+    return false;
+  });
+
 } // NodeList
