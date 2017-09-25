5cba654499baa69c7f26e6e3487a3d91456591f5
Daniel Kontšek <daniel@kontsek.sk>
2017-09-25 12:42:22 +0200

    Sort backup definitions by the schedule column (#272)
    Added ability to sort backup definitions by the schedule column

---
+++ gui/static/3rd-party/dataTables/js/jquery.dataTables.sorting.crontab.js
@@ -0,0 +1,29 @@
+jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+  'crontab-pre': function(a) {
+    var crontab_entry = a.split(' ');
+    var result = '';
+    var item;
+    var i;
+
+    // Use only minute and hour and ignore the rest
+    for (i=1; i >= 0; i--) {
+      item = crontab_entry[i].match(/(\d+)/);
+
+      if (item) {
+        result += (item[1] || '0').padStart(2, '0');
+      } else {
+        result += '00';
+      }
+    }
+
+    return parseInt(result) || 0;
+  },
+
+	'crontab-asc': function(a, b) {
+		return a - b;
+	},
+
+	'crontab-desc': function(a, b) {
+		return b - a;
+	}
+});
