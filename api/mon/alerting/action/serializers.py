ccbdc836a5297ca646c29b9638893dc733184aba
Pavol Kincel <secult@gmail.com>
2017-11-20 11:45:49 +0100

    Alerting action create stub created

--- api/mon/alerting/action/serializers.py
+++ api/mon/alerting/action/serializers.py
@@ -1,4 +1,71 @@
+from django.core.validators import RegexValidator
+
+from api import serializers as s
+from api.mon import MonitoringBackend
 
 
-class ActionSerializer(object):
-    pass
+DEFAULT_ACTION_MESSAGE_SUBJECT = "{TRIGGER.STATUS}: {TRIGGER.NAME}"
+
+DEFAULT_ACTION_MESSAGE = """Trigger: {TRIGGER.NAME}
+Trigger status: {TRIGGER.STATUS}
+Trigger severity: {TRIGGER.SEVERITY}
+Trigger URL: {TRIGGER.URL}
+
+Item values:
+
+1. {ITEM.NAME1} ({HOST.NAME1}:{ITEM.KEY1}): {ITEM.VALUE1}
+2. {ITEM.NAME2} ({HOST.NAME2}:{ITEM.KEY2}): {ITEM.VALUE2}
+3. {ITEM.NAME3} ({HOST.NAME3}:{ITEM.KEY3}): {ITEM.VALUE3}
+
+Original event ID: {EVENT.ID}
+"""
+
+
+class ActionSerializer(s.Serializer):
+    hostgroups = s.ArrayField(max_items=16384, default=[], validators=(
+                                             RegexValidator(regex=MonitoringBackend.RE_MONITORING_HOSTGROUPS),),
+                              required=True)
+
+    usergroups = s.ArrayField(max_items=16384, default=[], validators=(
+                                             RegexValidator(regex=MonitoringBackend.RE_MONITORING_HOSTGROUPS),),
+                              required=True)
+
+    message_subject = s.SafeCharField(max_length=65536, default=DEFAULT_ACTION_MESSAGE_SUBJECT)  # TODO initial value zo zabbixu vykopirovat a dat do settings
+    message_text = s.SafeCharField(max_length=65536, default=DEFAULT_ACTION_MESSAGE)  # TODO initial value zo zabbixu vykopirovat a dat do settings
+    recovery_message_enabled = s.BooleanField(default=False, required=False)
+    recovery_message_text = s.SafeCharField(max_length=65536, required=False)  # TODO initial value zo zabbixu vykopirovat, conditional validation
+
+    def validate_hostgroups(self, attrs, source):
+        # Allow to use only available hostgroups
+        return attrs
+        raise NotImplementedError
+        try:
+            value = attrs[source]
+        except KeyError:
+            pass
+        else:
+            if self.object and self.object.monitoring_hostgroups == value:
+                return attrs
+            elif self.dc_settings.MON_ZABBIX_HOSTGROUPS_VM_RESTRICT and not \
+                    set(value).issubset(set(self.dc_settings.MON_ZABBIX_HOSTGROUPS_VM_ALLOWED)):
+                raise s.ValidationError(_('Selected monitoring hostgroups are not available.'))
+
+        return attrs
+
+    def validate_usergroups(self, attrs, source):
+        # Allow to use only available hostgroups
+        return attrs
+
+        raise NotImplementedError
+        try:
+            value = attrs[source]
+        except KeyError:
+            pass
+        else:
+            if self.object and self.object.monitoring_hostgroups == value:
+                return attrs
+            elif self.dc_settings.MON_ZABBIX_HOSTGROUPS_VM_RESTRICT and not \
+                    set(value).issubset(set(self.dc_settings.MON_ZABBIX_HOSTGROUPS_VM_ALLOWED)):
+                raise s.ValidationError(_('Selected monitoring hostgroups are not available.'))
+
+        return attrs
--- api/mon/alerting/action/views.py
+++ api/mon/alerting/action/views.py
@@ -1,9 +1,13 @@
+from api.mon.alerting.action.serializers import ActionSerializer
+
 from api.mon.base.api_views import _MonBaseView
 
 from api.api_views import APIView
 from api.decorators import api_view, request_data, setting_required
 from api.mon.alerting.action.tasks import mon_action_list
 from api.permissions import IsAdmin
+from api.status import HTTP_201_CREATED
+from api.task.response import SuccessTaskResponse, FailureTaskResponse
 
 
 class MonActionView(_MonBaseView):
@@ -19,14 +23,24 @@ def action_list(request, data=None):
 
 
 class ActionView(APIView):
+    serializer = ActionSerializer
+
     def __init__(self, request, name, data):
         super(ActionView, self).__init__(request)
+        self.data = data
+        self.name = name
 
     def get(self):
         return
 
     def post(self):
-        return
+        ser = self.serializer(data=self.data)
+        ser.request = self.request
+        if ser.is_valid():
+            # push
+            return SuccessTaskResponse(self.request, ser.data, status=HTTP_201_CREATED, detail_dict=ser.detail_dict())
+        else:
+            return FailureTaskResponse(self.request, ser.errors)
 
     def put(self):
         return
