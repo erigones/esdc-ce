60b857b658fba2c0356cfce9680f4f93e816595e
Richard Kellner <richard.kellner@gmail.com>
2017-12-14 14:33:02 +0100

    Fixed filtering in gui in issue #95

--- gui/mon/forms.py
+++ gui/mon/forms.py
@@ -1,14 +1,11 @@
-import time
-
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+from gui.forms import SerializerForm
 from api.mon.alerting.serializers import AlertSerializer
 
 
-class BaseAlertFilterForm(forms.Form):
-    _ser = None
-
+class BaseAlertFilterForm(SerializerForm):
     since = forms.DateField(label=_('Since'), required=False, input_formats=('%Y-%m-%d', ''),
                                 widget=forms.DateInput(format='%Y-%m-%d',
                                                        attrs={'placeholder': _('Since'),
@@ -24,10 +21,6 @@ class BaseAlertFilterForm(forms.Form):
                                         'placeholder': _('Limit alerts to fetch'),
                                         'class': 'fill-up input-navigation input-transparent'}))
 
-    show_events = forms.BooleanField(label=_('Show Events'), required=False,
-                                        widget=forms.CheckboxInput(
-                                            attrs={'class': 'checkbox fill-up input-navigation'}))
-
     vm_hostnames = forms.CharField(label=_('Hostnames'), required=False, max_length=2048,
                                    widget=forms.TextInput(attrs={
                                        'placeholder': _('Hostnames'),
@@ -38,39 +31,69 @@ class BaseAlertFilterForm(forms.Form):
                                          'placeholder': _('Node hostname'),
                                          'class': 'fill-up input-navigation input-transparent'}))
 
-    dc_bound = forms.BooleanField(label=_('Filter as DC unbound'), required=False,
+    dc_bound = forms.BooleanField(label=_('Filter as DC bound'), required=False,
                                   widget=forms.CheckboxInput(attrs={'class': 'checkbox fill-up input-navigation'}))
 
-    def __init__(self, request, data, **kwargs):
-        self.request = request
-        super(BaseAlertFilterForm, self).__init__(data, **kwargs)
+    show_events = forms.BooleanField(label=_('Show Events'), required=False,
+                                        widget=forms.CheckboxInput(
+                                            attrs={'class': 'checkbox fill-up input-navigation'}))
 
-        if not self.request.user.is_staff:  # SuperAdmin only fields
+    def __init__(self, request, *args, **kwargs):
+        super(BaseAlertFilterForm, self).__init__(request, None, *args, **kwargs)
+        self.api_data = {}
+
+        if not request.user.is_staff:  # SuperAdmin only fields
             self.fields.pop('node_hostnames')
             self.fields.pop('dc_bound')
 
-    @staticmethod
-    def convert_to_timestamp(s):
-        try:
-            time_struct = time.strptime(str(s), '%Y-%m-%d')
-        except ValueError:
-            return s
-
-        return int(time.mktime(time_struct))
-
     def clean(self):
-        if not self.errors:
+        if not self._errors:
+            data = self.cleaned_data.copy()
 
-            if self.cleaned_data['since']:
-                self.cleaned_data['since'] = self.convert_to_timestamp(self.cleaned_data['since'])
+            if data['since']:
+                data['since'] = data['since'].strftime('%s')
 
-            if self.cleaned_data['until']:
-                self.cleaned_data['until'] = self.convert_to_timestamp(self.cleaned_data['until'])
+            if data['until']:
+                data['until'] = data['until'].strftime('%s')
 
-            self._ser = ser = AlertSerializer(self.request, data=self.cleaned_data)
-            if not ser.is_valid():
-                self._errors = ser.errors
+            data = self.remove_empty_fields(data)
+            ser = AlertSerializer(self._request, data=data)
 
-                return ser.data
+            if ser.is_valid():
+                self.api_data = ser.data
+
+                if not self._request.user.is_staff:  # SuperAdmin only fields
+                    self.api_data.pop('node_hostnames')
+                    self.api_data.pop('node_uuids')
+                    self.api_data.pop('dc_bound')
+
+                self.api_data = self.remove_empty_fields(self.api_data)
+            else:
+                self._set_api_errors(ser.errors)
 
         return super(BaseAlertFilterForm, self).clean()
+
+    @staticmethod
+    def remove_empty_fields(data):
+        cleaned_data = {}
+
+        for attribute in data:
+            if data[attribute] not in (None, [], ''):
+                cleaned_data[attribute] = data[attribute]
+
+        return cleaned_data
+
+    @classmethod
+    def format_data(cls, data):
+        formated_data = data.dict()
+        formated_data = cls.remove_empty_fields(formated_data)
+
+        if 'vm_hostnames[]' in formated_data:
+            formated_data['vm_hostnames'] = data.getlist('vm_hostnames[]')
+            formated_data.pop('vm_hostnames[]')
+
+        if 'node_hostnames[]' in formated_data:
+            formated_data['node_hostnames'] = data.getlist('node_hostnames[]')
+            formated_data.pop('node_hostnames[]')
+
+        return formated_data
--- gui/mon/views.py
+++ gui/mon/views.py
@@ -1,16 +1,14 @@
-import json
 from logging import getLogger
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 from gui.mon.forms import BaseAlertFilterForm
-from gui.utils import collect_view_data, get_pager
+from gui.utils import collect_view_data
 from gui.decorators import ajax_required, profile_required, admin_required
 from api.decorators import setting_required
 from api.utils.views import call_api_view
 from api.mon.alerting.views import mon_alert_list
-from api.task.utils import get_task_status
 
 logger = getLogger(__name__)
 
@@ -27,31 +25,41 @@ def monitoring_server(request):
 
 
 @login_required
-@ajax_required
+@admin_required
 @profile_required
+@ajax_required
 def alert_list_table(request):
     context = collect_view_data(request, 'mon_alert_list')
-    result, status = get_task_status(request.GET['task_id'])
+    context['show_events'] = False
 
-    context['alerts'] = result['result']
-    # context['alerts'] = context['pager'] = get_pager(request, result['result'])
+    if request.GET['show_events'] in ('true', 'on'):
+        context['show_events'] = request.GET['show_events']
 
-    if request.GET['show_events'] in (1, 'on', 'true', 'True'):
-        context['show_events'] = True
+    method = 'GET'
+    logger.info('Calling API view %s mon_alert_list(%s, data=%s) by user %s in DC %s',
+                method, request, None, request.user, request.dc)
+
+    res = call_api_view(request, method, mon_alert_list, data=BaseAlertFilterForm.format_data(request.GET))
+
+    if res.status_code in (200, 201) and method == 'GET' and res.data['result'] is not None:
+        context['alerts'] = res.data['result']
 
     return render(request, 'gui/mon/alert_table.html', context)
 
 
 @login_required
+@admin_required
 @profile_required
 def alert_list(request):
     context = collect_view_data(request, 'mon_alert_list')
-    # context['pager'] = get_pager(request, ())
     context['filters'] = alert_form = BaseAlertFilterForm(request, request.GET.copy())
-    clean_data = alert_form.clean()
+    alert_form.full_clean()
 
-    context['alert_filter'] = json.dumps(clean_data)
-    context['show_events'] = clean_data['show_events']
+    if not alert_form.has_changed() or alert_form.is_valid():  # new visit, or form submission
+        context['alert_filter'] = alert_form.api_data
+        context['show_events'] = alert_form.api_data['show_events']
+    else:
+        context['alert_filter'] = None  # Do not run javascript API TASKs!
 
     return render(request, 'gui/mon/alert_list.html', context)
 
