b44c208e99f0ad070773269240434f95228b5c89
YanChii <jan.poctavek@erigones.com>
2018-01-23 02:21:07 +0100

    minor fixes to esdc-overlay script

--- bin/debug/ipsec_logging_enable.sh
+++ bin/debug/ipsec_logging_enable.sh
@@ -1,1 +1,8 @@
 ndd -set ip ipsec_policy_log_interval 1
+ikeadm set debug all
+cat << EOF
+
+IPSec debugging enabled. See these logfiles:
+/var/log/in.iked.log (for phase 1 and 2)
+/var/adm/messages    (for packet drops)
+
