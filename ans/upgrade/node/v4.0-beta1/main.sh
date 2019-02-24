85931070aeba48384edc3d22be59917af9c4e127
Daniel Kontsek <daniel@kontsek.sk>
2019-02-24 13:20:59 +0100

    Fixed CPU monitoring script Related to erigones/esdc-factory#122 ( erigones/esdc-ce#375 )

--- ans/upgrade/node/v4.0-beta1/main.sh
+++ ans/upgrade/node/v4.0-beta1/main.sh
@@ -8,3 +8,9 @@ VERSION_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
 echo "+ Updating 020-ipsec-restore.sh"
 cp -f "${VERSION_DIR}/files/020-ipsec-restore.sh" "/opt/custom/etc/rc-pre-network.d/020-ipsec-restore.sh"
 
+#
+# https://github.com/erigones/esdc-ce/issues/375
+#
+echo "+ Updating /opt/zabbix/etc/scripts/vm-cpu-monitor"
+cat "${VERSION_DIR}/files/monitoring/vm-cpu-monitor" > "/opt/zabbix/etc/scripts/vm-cpu-monitor"
+svcadm restart svc:/application/zabbix/vm-cpu-monitor
