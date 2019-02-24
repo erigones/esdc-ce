8d86ff0b1f3d5a87a73cbed524c84cd43a32f1dd
Jan Poctavek <janci@binaryparadise.com>
2019-02-24 14:21:46 +0100

    fix executable bits (#414)

--- ans/upgrade/node/v4.0-beta1/main.sh
+++ ans/upgrade/node/v4.0-beta1/main.sh
@@ -7,6 +7,7 @@ VERSION_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
 
 echo "+ Updating 020-ipsec-restore.sh"
 cp -f "${VERSION_DIR}/files/020-ipsec-restore.sh" "/opt/custom/etc/rc-pre-network.d/020-ipsec-restore.sh"
+chmod +x "/opt/custom/etc/rc-pre-network.d/020-ipsec-restore.sh"
 
 #
 # https://github.com/erigones/esdc-ce/issues/375
