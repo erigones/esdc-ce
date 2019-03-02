bbd4c0dc6ab427bf1162b992bffb9b3e015b491c
Daniel Kontsek <daniel@kontsek.sk>
2019-03-02 14:35:06 +0100

    Changed value of VM's default PTR record to be a its hostname Closed #390

--- core/settings.py
+++ core/settings.py
@@ -435,7 +435,7 @@ DNS_MGMT_DOMAIN = 'local'  # Internal setting related to initial_data
 DNS_HOSTMASTER = 'hostmaster@example.com'  # This will be used as a placeholder for generating SOA records
 DNS_NAMESERVERS = ['ns1.' + DNS_MGMT_DOMAIN]  # Items will be used as a placeholder for generating SOA and NS records
 DNS_SOA_DEFAULT = '{nameserver} {hostmaster} 2013010100 28800 7200 604800 86400'  # For auto-generated SOA records
-DNS_PTR_DEFAULT = 'ptr-{ipaddr}.example.com'  # Content for the per VM PTR records.
+DNS_PTR_DEFAULT = '{hostname}'  # Content of VM's PTR record
 DNS_DOMAIN_TYPE_DEFAULT = 'MASTER'
 
 ESLIC_ENABLED = False  # Module. Enterprise Edition.
