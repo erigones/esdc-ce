6d04629b358b78a9faf798b90929fff46229ee28
YanChii <janci@binaryparadise.com>
2019-03-20 00:26:11 +0000

    added service upgrade tasks to 4.0-beta2

---
+++ ans/upgrade/node/v4.0-beta2/main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+ERIGONES_HOME="${ERIGONES_HOME:-"/opt/erigones"}"
+VERSION_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+
+echo "+ Updating 020-ipsec-restore.sh"
+cp -f "${VERSION_DIR}/files/020-ipsec-restore.sh" "/opt/custom/etc/rc-pre-network.d/020-ipsec-restore.sh"
+chmod +x "/opt/custom/etc/rc-pre-network.d/020-ipsec-restore.sh"
+
+#
+# https://github.com/erigones/esdc-factory/pull/128
+#
+echo "+ Updating /opt/custom/smf/zabbix-agent.xml"
+cat "${VERSION_DIR}/files/zabbix-agent.xml" > "/opt/custom/smf/zabbix-agent.xml"
