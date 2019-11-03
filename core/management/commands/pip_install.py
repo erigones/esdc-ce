9e28b4d94709cd3effbc0cf4811861b2f4b6128d
YanChii <janci@binaryparadise.com>
2019-11-03 21:47:07 +0000

    add custom include dir for pip builds

--- core/management/commands/pip_install.py
+++ core/management/commands/pip_install.py
@@ -1,4 +1,5 @@
 from ._base import DanubeCloudCommand, CommandOption, lcd
+import os
 
 PROJECT_DIR = DanubeCloudCommand.PROJECT_DIR
 
@@ -14,6 +15,7 @@ class Command(DanubeCloudCommand):
     req_file_both = DanubeCloudCommand._path(PROJECT_DIR, 'etc', 'requirements-both.txt')
     req_file_mgmt = DanubeCloudCommand._path(PROJECT_DIR, 'etc', 'requirements-mgmt.txt')
     req_file_node = DanubeCloudCommand._path(PROJECT_DIR, 'etc', 'requirements-node.txt')
+	erigones_home = os.path.abspath(os.environ.get('ERIGONES_HOME', '/opt/erigones'))
 
     def pip_install(self, req_path, params=''):
         if self._path_exists(req_path):
@@ -32,6 +34,9 @@ class Command(DanubeCloudCommand):
             params = '-U'
         else:
             params = ''
+
+		# add include dir for pip builds
+        params+=' --global-option=build_ext --global-option="-I%s/include/"' % erigones_home
 
         with lcd(self.PROJECT_DIR):
             self.pip_install(self.req_file_both, params=params)
