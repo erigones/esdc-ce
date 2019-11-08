65927b8128cb9b6e433cce7a11ad9ee8176d39a2
Jan Poctavek <janci@binaryparadise.com>
2019-11-08 18:58:14 +0100

    Fix rpm conflict during python3 install on some installations (#470)
    * update python-devel before installing python3 during upgrade
    * add usb mount scripts to bin

--- ans/upgrade/node/v4.2/main.sh
+++ ans/upgrade/node/v4.2/main.sh
@@ -6,14 +6,6 @@ ERIGONES_HOME="${ERIGONES_HOME:-"/opt/erigones"}"
 VERSION_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
 
 #
-# https://github.com/erigones/esdc-ce/issues/450
-#
-echo "+ Updating /usbkey/scripts/*usb* scripts"
-cat "${VERSION_DIR}/files/mount-usb.sh" > "/opt/custom/smf/mount-usb.sh"
-cat "${VERSION_DIR}/files/umount-usb.sh" > "/opt/custom/smf/umount-usb.sh"
-chmod 755 "/opt/custom/smf/mount-usb.sh" "/opt/custom/smf/umount-usb.sh"
-
-#
 # https://github.com/erigones/esdc-ce/issues/446
 #
 echo "+ Updating Zabbix agent helpers"
