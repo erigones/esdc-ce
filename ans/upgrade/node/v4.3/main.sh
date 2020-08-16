b67eee82a678e2312e30d0a1b3d12e94ac9ecfa9
Jan Poctavek <janci@binaryparadise.com>
2020-08-16 14:13:39 +0000

    fix typo

--- ans/upgrade/node/v4.3/main.sh
+++ ans/upgrade/node/v4.3/main.sh
@@ -10,5 +10,5 @@ VERSION_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
 # (Clean installs were not fixed so check and re-run fix from previous version.)
 #
 if ! grep -q LD_LIBRARY_PATH /opt/custom/smf/zabbix-vm-network-monitor.xml; then
-	"${VERSION_DIR}/../v4.2/main.sh
+	"${VERSION_DIR}/../v4.2/main.sh"
 fi
