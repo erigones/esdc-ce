a4f205d2aa19fd63c6173bd568e6625b44d1fecb
Jan Poctavek <janci@binaryparadise.com>
2020-08-16 14:05:43 +0000

    fix LD_LIBRARY_PATH in node zabbix agents (omitted in v4.2 clean installs)

---
+++ ans/upgrade/node/v4.3/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+ERIGONES_HOME="${ERIGONES_HOME:-"/opt/erigones"}"
+VERSION_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+
+#
+# https://github.com/erigones/esdc-ce/issues/446
+# (Clean installs were not fixed so check and re-run fix from previous version.)
+#
+if ! grep -q LD_LIBRARY_PATH /opt/custom/smf/zabbix-vm-network-monitor.xml; then
+	../v4.2/main.sh
+fi
