73398b96e2ed2fa29565538f34c608cd9f599cbf
Jan Poctavek <janci@binaryparadise.com>
2020-11-12 00:08:19 +0100

    Add classless delegated reverse DNS zones (#517)
    Fixes #507

--- vms/models/vm.py
+++ vms/models/vm.py
@@ -2003,6 +2003,12 @@ class Vm(_StatusModel, _JsonPickleModel, _OSType, _UserTasksModel):
 
         return vm_nics
 
+    def get_nic_by_ip(self, ip):
+        for nic in self.get_vm_nics():
+            if nic['ip'] == ip:
+                return nic
+        return None
+
     def get_network_uuids(self):
         """Return set of network_uuids for currently used/required subnets by this VM"""
         return {nic['network_uuid'] for nic in self.get_vm_nics()}
