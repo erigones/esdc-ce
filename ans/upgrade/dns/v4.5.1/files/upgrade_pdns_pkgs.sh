03046e2f8aa90bd5482a417fcf1e0ede8fe9dc4e
Jan Poctavek <janci@binaryparadise.com>
2021-07-24 19:55:56 +0200

    stabilize upgrade of dns01 appliance

--- ans/upgrade/dns/v4.5.1/files/upgrade_pdns_pkgs.sh
+++ ans/upgrade/dns/v4.5.1/files/upgrade_pdns_pkgs.sh
@@ -1,21 +1,60 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -x
 set -e
 
-# "pkgin upgrade" will not downgrade packages so it is future-safe even on re-exec
+declare -A PKGS
+PKGS[powerdns]=4.4.0
+PKGS[powerdns-pgsql]=4.4.0
+PKGS[powerdns-recursor]=4.4.2
+PKGS[dnsdist]=1.5.1
 
-#/opt/local/bin/pkgin -y install powerdns-4.2.2 powerdns-pgsql-4.2.2 powerdns-recursor-4.3.1 dnsdist-1.3.3nb1
-/opt/local/bin/pkgin -y upgrade powerdns-4.4.0 powerdns-pgsql-4.4.0 powerdns-recursor-4.4.2 dnsdist-1.5.1
-set +e
+SERVICES="pdns pdns-recursor pdns-dnsdist"
 
-/opt/local/bin/timeout 100 /usr/sbin/svcadm disable -st pdns pdns-recursor pdns-dnsdist
+### START ###
 
-# recursor tends to hang after upgrade
-if pkill -0 pdns_recursor; then
-        pkill -9 pdns_recursor
+DC_REPO="$(cat /opt/local/etc/pkgin/repositories.conf | grep -i danube)"
+
+if [ "$(wc -l <<< "${DC_REPO}")" -ne 1 ]; then
+	echo "ERROR: Danube repo was not found in pkgin repo list"
+	exit 10
 fi
 
-for svc in pdns pdns-recursor pdns-dnsdist; do
+PKG_LIST="$(pkg_info)"
+for pkg in "${!PKGS[@]}"; do
+	if ! grep "^${pkg}-[0-9]" <<< "${PKG_LIST}"; then
+		echo "Warning: package '${pkg}' is not installed"
+	fi
+done
+
+set -x
+
+cd /tmp
+for pkg in "${!PKGS[@]}"; do
+	pkg_fullname="${pkg}-${PKGS[$pkg]}"
+	/opt/local/bin/curl -sO "${DC_REPO}/${pkg_fullname}"
+done
+
+# stop services before upgrade
+/opt/local/bin/timeout 100 /usr/sbin/svcadm disable -st ${SERVICES}
+
+set +e	# uninstall can fail
+# remove pkgs
+for pkg in "${!PKGS[@]}"; do
+	/opt/local/sbin/pkg_delete "${pkg}"
+done
+set -e
+
+# install new pkgs
+for pkg in "${!PKGS[@]}"; do
+	pkg_fullname="${pkg}-${PKGS[$pkg]}"
+	/opt/local/sbin/pkg_add "${pkg_fullname}"
+	rm "${pkg_fullname}"
+done
+
+
+# start services back
+for svc in ${SERVICES}; do
 	/opt/pdns-confd/contrib/smartos-reload-smf.sh restart "${svc}"
 done
+
+### END ###
